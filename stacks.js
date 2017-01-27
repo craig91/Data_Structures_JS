78c8e1ece7a6bd2de43b1b37563dfb401a149c97
craig91 <craigdejean1>
2017-01-27 00:21:25 -0500

    added push() function

--- stacks.js
+++ stacks.js
@@ -1,7 +1,17 @@
+// What is a stack? It is a list of elements that are accessible only from one end of the list.
+// That end of the list is called the top. The stack is known as LIFO (Last-in Firs-out)
+
+// Because of the LIFO nature, any element that is not currently at the top of the stack cannot be accessed.
+// In order to get to the element you want, you have to get rid of all the other elements above it first.
+
 function Stack() {
-  this.dataStore = [];
-  this.top = 0;
-  this.push = push;
-  this.pop = pop;
-  this.peek = peek;
+  this.dataStore = []; // Stores the stack elements.
+  this.top = 0; // keeps track of whats at the top of the stack
+  this.push = push; // pushes an element to the top of the stack
+  this.pop = pop; // removes an element from the top of the stack
+  this.peek = peek; // returns an element from the stack and returns it.
+}
+
+function push (element) {
+  this.dataStore[this.top++] = element;
 }
