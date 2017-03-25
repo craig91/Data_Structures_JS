b44c7aa5bbec6a46f36dee0775bfcd2621006035
craig91 <craigdejean1@gmail.com>
2017-03-24 23:48:54 -0400

    hash table explanation

---
+++ hash_table.js
@@ -0,0 +1,10 @@
+// Hashing is a common technique for storing data in such a way that the data can be inserted and retrieved very qui
+// quickly.
+
+// Hashing uses a data stuctrue called a hash table.
+// Hash tables perform poorly for operations that involve searching, like searching for minimum or max values in a data set.
+
+
+// The hash table data structure is designed around an array. The array consists of elements 0 through some pre-determined size, though the size can be increased when necessary.
+// Each data element is stored in the array based on an associated data element called the key. To store a piece of data in a hash table, the key is mapped into a number in the range of 0 through the hash table size, using a
+// hash function. 
